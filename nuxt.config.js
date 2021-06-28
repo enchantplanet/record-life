c6363fc7943be6f5f1c4cad9919e739b62562f35
H2008077 <A933391@ict-companion.com>
2021-06-28 16:00:38 +0900

    수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.NODE_ENV !== 'production' ? process.env.API_URL : process.env.API_URL2,
+    baseURL: "/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
